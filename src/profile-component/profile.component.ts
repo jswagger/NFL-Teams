e9c61355137eb21dffe97a43236a8b4f0f0adc1e
Jeremy Swagger <jeremy.swagger@gmail.com>
2017-12-22 12:49:37 -0600

    starts building profile as component

--- src/profile-component/profile.component.ts
+++ src/profile-component/profile.component.ts
@@ -1,27 +1,75 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'profile-container',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
-export class AppComponent {
-  title: string;
-  sidebarOpen: string;
-
+export class ProfileComponent {
+  public teamData: any;
+  public currentLogo: string;
+  public currentConf: string;
+  
   constructor() {
+    this.teamData = {};
+    this.currentLogo = "";
+    this.currentConf = "";
   }
   ngOnInit(){
-    this.title = '32 Teams';
-    this.sidebarOpen = "inline-block";
+  };
 
-  };
-  public toggleSidebar() {
-    if (this.sidebarOpen = "false"){
-      this.sidebarOpen = "true";
-    };
-    if (this.sidebarOpen = "true"){
-      this.sidebarOpen = "false";
-    };
-  };
+  public setTeamData(data: object, id: string){
+    this.teamData = data;
+    this.currentLogo = "/assets/images/" + id + "_logo.png";
+    this.currentConf = "/assets/images/" + this.teamData.conference + "_logo.png";
+  }
+  public getTeamInfo(id) {
+    if (id = "MIN") {
+      return {
+        "pager": {},
+        "data": [
+            {
+                "branding": {
+                    "logos" : [
+                        {
+                            "href" : "http://path.to/thumb/logo.jpg",
+                            "size" : "THUMB"
+                        },
+                        {
+                            "href" : "http://path.to/small/logo.jpg",
+                            "size" : "SMALL"
+                        },
+                        {
+                            "href" : "http://path.to/medium/logo.jpg",
+                            "size" : "MEDIUM"
+                        },
+                        {
+                            "href" : "http://path.to/mezzanine/logo.jpg",
+                            "size" : "MEZZANINE"
+                        }
+                    ]
+                },
+                "fullName": "Minnesota Vikings",
+                "conference": "NFC",
+                "division": "NCN",
+                "webDomain": "http://www.vikings.com",
+                "established": 1960,
+                "venue": {
+                    "name":"US Bank Stadium",
+                    "ticketPhoneNumber" : "832-667-2390",
+                    "ticketUrl" : "http://www.nfl.com/tickets/houston-texans"
+                },
+                "standings" : {
+                    "overallWins":11,
+                    "overallLosses":3,
+                    "overallTies":0,
+                    "divisionRank":1
+                }
+            }
+        ]
+     }
+    }
+  }
+
+
 }
