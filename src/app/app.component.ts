c1f4687102f2cc4ad9dd0172a013fcecef09fc05
Jeremy Swagger <jeremy.swagger@gmail.com>
2018-01-12 13:01:01 -0600

    Updates to start view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent {
   public teamProfile: ProfileComponent;
   public currentTeam: any;
   public carouselOne: NgxCarousel;
+  public startView: boolean;
   
   constructor() {
   }
@@ -30,6 +31,7 @@ export class AppComponent {
     this.divisionsList = this.getDivisionsList();
     this.selectedTeamObject = {};
     this.carouselOne = this.startCarousel();
+    this.startView = true;
     
   };
   public toggleSidebar() {
@@ -122,7 +124,34 @@ export class AppComponent {
       speed: 400,
       interval: 4000,
       point: {
-        visible: true
+        visible: true,
+        pointStyles: `
+          .ngxcarouselPoint {
+            list-style-type: none;
+            text-align: center;
+            padding: 12px;
+            margin: 0;
+            white-space: nowrap;
+            overflow: auto;
+            position: absolute;
+            width: 100%;
+            bottom: 20px;
+            left: 0;
+            box-sizing: border-box;
+          }
+          .ngxcarouselPoint li {
+            display: inline-block;
+            border-radius: 999px;
+            background: rgba(255, 255, 255, 0.55);
+            padding: 5px;
+            margin: 0 3px;
+            transition: .4s ease all;
+          }
+          .ngxcarouselPoint li.active {
+              background: white;
+              width: 10px;
+          }
+        `
       },
       load: 2,
       touch: true,
