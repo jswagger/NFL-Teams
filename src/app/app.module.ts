0899fa3e5d9cff30f6e82f9d96d9e989f3dbee43
Jeremy Swagger <jeremy.swagger@gmail.com>
2018-04-05 09:17:38 -0500

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatSelectModule, MatCardModule, MatGridListModule, MatSidenavModule} from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { NgxCarouselModule } from 'ngx-carousel';
-
-
 import { AppComponent } from './app.component';
 import { TeamListComponent } from './team-list/team-list.component';
 import { TeamProfileComponent } from './team-profile/team-profile.component'
